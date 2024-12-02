8c43eb90721c91e8b424b2ed6c0d5b7f783ae4bd
Deepak Kumbhar <deepakkumbhar2006@gmail.com>
2024-12-02 09:37:53 +0530

    Update problem 344 - leetcode.java

--- Recursion/Problems/reverse.java
+++ Recursion/Problems/reverse.java
@@ -8,16 +8,26 @@ public class reverse {
         reverseString(s);
     }
     public static void reverseString(char[] s){
-        change(s, 0, s.length-1);
+        method1(s, 0, s.length-1);
         System.out.println(Arrays.toString(s));
     }
-    public static void change(char[] s,int start, int end){
+    public static void method1(char[] s,int start, int end){
         if (start==end){
             return;
         }
         char temp = s[start];
         s[start] = s[end];
         s[end] = temp;
-        change(s, start+1, end-1);
+        method1(s, start+1, end-1);
+    }
+    // ------- Method2/ 0 ms runtime/ best method --------
+    public void reverseString2(char[] s) {
+        if (s != null && s.length > 0) {
+            for (int l = 0, r = s.length - 1; l < r; l++, r--) {
+                char tmp = s[l];
+                s[l] = s[r];
+                s[r] = tmp;
+            }
+        }
     }
 }
