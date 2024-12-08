92d0a202e8ee0942bb5fddcad13e8d7e2a65df52
Deepak Kumbhar <deepakkumbhar2006@gmail.com>
2024-12-08 10:40:21 +0530

    LC contest- 427 Transformed_Array.java
    This problem teaches you how to move array in circular path

--- Problem not able to solved/Transformed_Array.java
+++ Problem not able to solved/Transformed_Array.java
@@ -2,15 +2,16 @@ package Problems;
 
 import java.util.Arrays;
 
+// This is an problem link : https://leetcode.com/problems/transformed-array/
 public class Transformed_Array {
     public static void main(String[] args) {
-        System.out.println(3%5);
-        int[] nums = {-17, 17, 0};
+        int[] nums = {-1, 4, -1};
         System.out.println(Arrays.toString(construcTransformedArray(nums)));
     }
     public static int[] construcTransformedArray(int[] nums){
         int n = nums.length;
         int[] target = new int[n];
+		// This for loop is very important to understand how to move array in circular path. 
         for (int i = 0; i <  n; i++) {
             int steps = nums[i];
             int targetIndex = (i+steps)%n;
@@ -22,3 +23,7 @@ public class Transformed_Array {
         return target;
     }
 }
+		
+// O/P
+// target = {-1, -1, 4}
+
