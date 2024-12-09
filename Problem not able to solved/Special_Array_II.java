5b7f03df391e66dc8c1471efbe2a61faa8f6ae6d
Deepak Kumbhar <deepakkumbhar2006@gmail.com>
2024-12-09 10:59:23 +0530

    POTD 3152 Special_Array_II.java

---
+++ Problem not able to solved/Special_Array_II.java
@@ -0,0 +1,38 @@
+class Solution {
+  /*
+  Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 105
+1 <= queries.length <= 105
+queries[i].length == 2
+0 <= queries[i][0] <= queries[i][1] <= nums.length - 1
+*/
+
+  // Time Limit Exceed 
+    public boolean[] isArraySpecial(int[] nums, int[][] queries) {
+        boolean[] lis = new boolean[queries.length];
+        for(int i = 0; i< lis.length; i++){
+            // if(nums.length==1){
+            //     lis[i] = true;
+            //     break;
+            // }
+            for (int j = 0; j < queries[i].length-1; j++) {
+                int from = queries[i][j];
+                int to = queries[i][j+1];
+                if(from==to){
+                    lis[i] = true;
+                    break;
+                }
+                for (int k = from; k < to ; k++) {
+                    if (nums[k]%2 == nums[k+1]%2){
+                        lis[i] = false;
+                        break;
+                    }
+                    else lis[i] = true;
+                }
+            }
+        }
+        return lis;
+    }
+}
