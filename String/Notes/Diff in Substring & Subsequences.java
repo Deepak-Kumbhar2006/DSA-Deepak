4206f6e9cee6db7ce78218926c764833a777ff60
Deepak Kumbhar <deepakkumbhar2006@gmail.com>
2024-12-10 22:05:18 +0530

    Update Diff in Substring & Subsequences.java

--- String/Notes/Diff in Substring & Subsequences.java
+++ String/Notes/Diff in Substring & Subsequences.java
@@ -2,3 +2,18 @@ Difference between Subsequences and substring
 String str = "abc";
 String[] Subsequences  = {"a",  "b", "c", "ab", "ac", "bc", "abc"}  // non contiguous   total = (2^n - 1)
 String[] Substring = {"a", "ab" ,"abc", "b", "bc", "c"}    // contiguous    total = n*(n-1)/2
+
+
+sorting a substring:
+String s = "abcaba";
+char[] temps.toCharArray();
+Arrays.sort(s.toCharArray());
+Arrays.sort(temp);
+String s_sort = new String(temp);
+System.out.println(s_sort);
+
+Q] Why can't we write this sorting string code of number of line in just one line like this: 
+
+String = new String(Arrays.sort(s.toCharArray()));
+
+Ans] Arrays.sort() does not have any return type. It is void method
