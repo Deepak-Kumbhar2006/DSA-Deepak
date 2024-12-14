9f72a1c8e5bd396a3e2c78b65706793f6e671238
Deepak Kumbhar <deepakkumbhar2006@gmail.com>
2024-12-14 10:50:31 +0530

    Update Diff in Substring & Subsequences.java

--- String/Notes/Diff in Substring & Subsequences.java
+++ String/Notes/Diff in Substring & Subsequences.java
@@ -1,7 +1,7 @@
 Difference between Subsequences and substring
 String str = "abc";
 String[] Subsequences  = {"a",  "b", "c", "ab", "ac", "bc", "abc"}  // non contiguous   total = (2^n - 1)
-String[] Substring = {"a", "ab" ,"abc", "b", "bc", "c"}    // contiguous    total = n*(n-1)/2
+String[] Substring = {"a", "ab" ,"abc", "b", "bc", "c"}    // contiguous    total = n*(n+1)/2
 
 
 sorting a substring:
