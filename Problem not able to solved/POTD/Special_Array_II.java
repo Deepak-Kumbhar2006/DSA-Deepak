96803f44ab4691ae80323d2eb1b4ee9663e6e995
Deepak Kumbhar <deepakkumbhar2006@gmail.com>
2024-12-14 10:02:29 +0530

    Update Special_Array_II.java

--- Problem not able to solved/POTD/Special_Array_II.java
+++ Problem not able to solved/POTD/Special_Array_II.java
@@ -9,7 +9,7 @@ queries[i].length == 2
 0 <= queries[i][0] <= queries[i][1] <= nums.length - 1
 */
 
-  // Time Limit Exceed 
+  // Time Limit Exceed, Because this is brute force solution time complexity becomes - queries*10^5
     public boolean[] isArraySpecial(int[] nums, int[][] queries) {
         boolean[] lis = new boolean[queries.length];
         for(int i = 0; i< lis.length; i++){
@@ -35,4 +35,8 @@ queries[i].length == 2
         }
         return lis;
     }
+
+  // More optimize solution 
+  // first make a array of cummulitive sum 
+  // watch video of codestorywithMIK yt special array II
 }
