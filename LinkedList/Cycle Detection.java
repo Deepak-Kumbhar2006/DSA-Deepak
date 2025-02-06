66327e46bcaf4e9f908086e88ab37c5d50c20285
Deepak Kumbhar <deepakkumbhar2006@gmail.com>
2025-02-06 09:02:19 +0530

    Create  Cycle Detection.java

---
+++ LinkedList/Cycle Detection.java
@@ -0,0 +1,14 @@
+// Slow and fast pointer 
+//--------------------------Cycled LinkedList--------------------------------- 
+    public boolean hasCycle() {
+        Node fast = head;
+        Node slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
